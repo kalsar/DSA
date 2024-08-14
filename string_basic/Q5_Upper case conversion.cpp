2dfc10b7ac92295ece4221705931547ff7a26fbe
Kalsar <114978816+kalsar@users.noreply.github.com>
2024-08-14 13:19:16 +0530

    Create Q5_Upper case conversion.cpp

---
+++ string_basic/Q5_Upper case conversion.cpp
@@ -0,0 +1,37 @@
+/*
+Input:
+str = "i love programming"
+Output: "I Love Programming"
+Explanation:
+'I', 'L', 'P' are the first letters of 
+the three words.
+*/
+
+string transform(string s)
+{
+    // code here
+    string str="";
+    str.push_back(s[0]-32);
+    int i=1;
+    while(i<s.length() ){
+        
+         if(s[i]==' ' && i + 1 < s.length()){
+         str.push_back(s[i]);
+             
+            i++;
+            str.push_back(s[i]-32);
+        }
+        
+        else{
+            
+        str.push_back(s[i]);
+        }
+        
+        i++;
+        
+        
+    }
+    return str;
+    
+       
+    }
