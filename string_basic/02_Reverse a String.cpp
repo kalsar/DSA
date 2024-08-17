08195b0e014c3e245dfcddeaedd15c3349fd0ef8
Kalsar <114978816+kalsar@users.noreply.github.com>
2024-08-18 00:21:33 +0530

    Update 02_Reverse a String.cpp

--- string_basic/02_Reverse a String.cpp
+++ string_basic/02_Reverse a String.cpp
@@ -14,3 +14,17 @@ string revStr(string s) {
         }
         return str;
     }
+// second approach: with out extra space
+string reverseWord(string str)
+    {
+        int s=0;
+        int e=str.size()-1;
+        while(s<e){
+            char temp=str[s];
+            str[s]=str[e];
+            str[e]=temp;
+            s++;
+            e--;
+        }
+        return str;
+    }
