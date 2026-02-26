291d9219cf193f9513498ba2627826887e8a7124
Kalsar <114978816+kalsar@users.noreply.github.com>
2026-02-26 15:32:00 +0530

    Create FindMissingNumber.cpp

---
+++ Array/FindMissingNumber.cpp
@@ -0,0 +1,24 @@
+
+Approach 1:
+// This approach will faild when input is to large 
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        // code here
+    int n=arr.size()+1;
+        
+    int totalSum=(n*(n+1))/2;
+    int sum=0;
+    for(int i=0; i<n-1; i++){
+        sum=sum+arr[i];
+    }
+    return totalSum-sum;
+        
+    }
+};
+
+---------------------------------------------------------------------------------------------------------------------
+  approach 2:
