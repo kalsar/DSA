3ac6b3020f14a7d9ee8e17bb590d43c33eb32519
Kalsar <114978816+kalsar@users.noreply.github.com>
2026-02-27 07:18:31 +0530

    Update FindMissingNumber.cpp

--- Array/FindMissingNumber.cpp
+++ Array/FindMissingNumber.cpp
@@ -22,3 +22,20 @@ class Solution {
 
 ---------------------------------------------------------------------------------------------------------------------
   approach 2:
+  // using xor:
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n=nums.size();
+        int result=n;
+
+        for(int i=0; i<n; i++){
+            result^=i;
+            result^=nums[i];
+
+        }
+        return result;
+    }
+};
