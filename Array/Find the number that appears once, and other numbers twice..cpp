17836d50a1355adae236cca9ed050279d260d49f
Kalsar <114978816+kalsar@users.noreply.github.com>
2026-02-27 12:04:55 +0530

    Create Find the number that appears once, and other numbers twice..cpp

---
+++ Array/Find the number that appears once, and other numbers twice..cpp
@@ -0,0 +1,17 @@
+// optimal approach :
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+               int ans=0;
+     
+        for(int i=0; i<nums.size(); i++){
+            ans=ans^nums[i];
+
+
+        } 
+        return ans;
+        
+    }
+};
