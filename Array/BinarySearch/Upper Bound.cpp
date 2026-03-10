c3c89f480a9071376ece9252c27adfd5e5755424
Kalsar <114978816+kalsar@users.noreply.github.com>
2026-03-10 07:15:04 +0530

    Create Upper Bound.cpp

---
+++ Array/BinarySearch/Upper Bound.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+    int upperBound(vector<int> &nums, int x){
+      int n=nums.size();
+        int s=0;
+        int ans=n;
+        int e=n-1;
+  
+        while(s<=e){
+         int mid=s+(e-s)/2;
+            if(nums[mid]>x){
+                ans=mid;
+                e=mid-1;
+
+            }
+            else if(nums[mid]<=x){
+                s=mid+1;
+            }
+        }
+        return ans;
+        
+    }
+};
