c8d13fcb9eb5de3eca29f49d6390f83fc181671b
Kalsar <114978816+kalsar@users.noreply.github.com>
2026-03-14 07:18:56 +0530

    Create Find square root of a number.cpp

---
+++ Array/BinarySearch/Find square root of a number.cpp
@@ -0,0 +1,30 @@
+
+
+optimal :
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s=1;
+        int e=x;
+        int ans=0;
+        while(s<=e){
+            long long int mid=s+(e-s)/2;
+            long long int square=mid*mid;
+            if(square==x){
+                return mid;
+            }
+            else if(square<x){
+                ans=mid;
+                s=mid+1;
+
+            }
+            else{
+                e=mid-1;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
