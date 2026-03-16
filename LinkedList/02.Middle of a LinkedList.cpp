d671fdce32767a27a255cb9edaf023815c27508e
Kalsar <114978816+kalsar@users.noreply.github.com>
2026-03-16 06:19:15 +0530

    Create 02.Middle of a LinkedList.cpp

---
+++ LinkedList/02.Middle of a LinkedList.cpp
@@ -0,0 +1,32 @@
+// Leetcode :876
+// Brute force approach:
+First iterate whole linked list then find size of linkd list
+then return size/2+1 node 
+
+
+  // Approach two pointer fast and slow :
+
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+
+        
+    }
+};
